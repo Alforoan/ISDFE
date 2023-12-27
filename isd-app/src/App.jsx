0d5338ddaf7bb56e200691a0eb039c03f7715dd3
Hongkun LIU <66795576+lhkun123@users.noreply.github.com>
2023-12-26 19:28:40 -0800

    Added user invitation page and install "ant design" to implement components

--- isd-app/src/App.jsx
+++ isd-app/src/App.jsx
@@ -1,36 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  Home,
-  LogIn,
-  SignUp,
-  Request,
-  CourseRequest,
-  AccountSetUpEmail,
-  AccountSetUpNamePassword,
-  AccountSetUpCompanyName,
-} from "./pages";
+import Home from "./pages/Home/Home";
+import LogIn from "./pages/LogIn/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Request from "./pages/Request/Request";
+import CourseRequest from "./pages/CourseRequest/CourseRequest";
+import AccountSetUpEmail from "./pages/AccountSetUp/Email/Email";
+import AccountSetUpNamePassword from "./pages/AccountSetUp/NamePassword/NamePassword";
+import AccountSetUpCompanyName from "./pages/AccountSetUp/CompanyName/CompanyName";
+import AccountSetUpUsers from "./pages/AccountSetUp/AccountSetUpUsers/AccountSetUpUsers";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/request" element={<Request />} />
-        <Route path="/courserequest" element={<CourseRequest />} />
-        <Route path="/accountsetup/email" element={<AccountSetUpEmail />} />
-        <Route
-          path="/accountsetup/name_password"
-          element={<AccountSetUpNamePassword />}
-        />
-        <Route
-          path="/accountsetup/company_name"
-          element={<AccountSetUpCompanyName />}
-        />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/request" element={<Request />} />
+                <Route path="/courserequest" element={<CourseRequest />} />
+                <Route path="/accountsetup/email" element={<AccountSetUpEmail />} />
+                <Route
+                    path="/accountsetup/name_password"
+                    element={<AccountSetUpNamePassword />}
+                />
+                <Route
+                    path="/accountsetup/company_name"
+                    element={<AccountSetUpCompanyName />}
+                />
+                <Route
+                    path="/accountsetup/users"
+                    element={<AccountSetUpUsers />}
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
