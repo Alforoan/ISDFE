866e8d135eddc7c076007e82629dc8db8f974438
Anastasiya Padalitskaya <nastylia@gmail.com>
2024-06-08 20:06:40 -0700

    requests initial page

--- isd-app/src/pages/index.js
+++ isd-app/src/pages/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from './Home/Home';
 export { default as LogIn } from './LogIn/Login';
 export { default as SignUp } from './SignUp/SignUp';
-export { default as Request } from './Request/Request';
+export { default as Requests } from './Requests/Requests';
 export { default as CourseRequest } from './CourseRequest/CourseRequest';
 export { default as AccountSetUpEmail } from './AccountSetUp/Email/Email';
 export { default as AccountSetUpNamePassword } from './AccountSetUp/NamePassword/NamePassword';
