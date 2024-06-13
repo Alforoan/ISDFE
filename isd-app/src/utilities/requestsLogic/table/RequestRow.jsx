b033430de6dd0a876df7708226e2f8df205b2b86
Anastasiya Padalitskaya <nastylia@gmail.com>
2024-06-13 14:25:46 -0700

    The requests table is sorted the way the most recent Last Updated requests are on the top of the table

--- isd-app/src/utilities/requestsLogic/table/RequestRow.jsx
+++ isd-app/src/utilities/requestsLogic/table/RequestRow.jsx
@@ -1,13 +1,14 @@
 import "./RequestRow.scss";
 import RequestStatus from "./RequestStatus";
 import stages from "./stages";
+import DateFormatter from "./DateFormatter.js";
 
 const RequestRow = ({ request }) => {
   return (
     <tr className="request-row">
       <td className="request-name">{request.requestName}</td>
       <td className="assigned-to">{request.assignedTo}</td>
-      <td className="last-updated">{request.lastUpdated}</td>
+      <td className="last-updated">{DateFormatter(request.lastUpdated)}</td>
       <td className="stage">{stages[request.stage]}</td>
       <td className="status">
         <RequestStatus status={request.status} />
--- isd-app/src/utilities/requestsLogic/table/RequestsTable.jsx
+++ isd-app/src/utilities/requestsLogic/table/RequestsTable.jsx
@@ -3,36 +3,41 @@ import RequestRow from "./RequestRow";
 import RequestRowSmallScreen from "./RequestRowSmallScreen";
 import { useState, useEffect } from "react";
 
-const info = [
+let info = [
   {
     requestName: "Reducing issues in manufacturing workflow",
     assignedTo: "John Doe",
-    lastUpdated: "12 Apr, 2024",
+    lastUpdated: "Thu Feb 01 2024 00:00:00 GMT-0800 (Pacific Standard Time)",
     stage: "storyboard",
     status: "stakeholderReview",
   },
   {
     requestName: "Reducing issues in manufacturing workflow",
     assignedTo: "John Doe",
-    lastUpdated: "12 Apr, 2024",
+    lastUpdated: "Tue Nov 07 2023 00:00:00 GMT-0800 (Pacific Standard Time)",
     stage: "needsAnalysis",
     status: "qaReview",
   },
   {
     requestName: "Reducing issues in manufacturing workflow",
     assignedTo: "John Doe",
-    lastUpdated: "12 Apr, 2024",
+    lastUpdated: "Thu Jun 13 2024 13:29:28 GMT-0700 (Pacific Daylight Time)",
     stage: "objectives",
     status: "status",
   },
   {
     requestName: "Reducing issues in manufacturing workflow",
     assignedTo: "John Doe",
-    lastUpdated: "12 Apr, 2024",
+    lastUpdated: "Fri Apr 12 2024 00:00:00 GMT-0700 (Pacific Daylight Time)",
     stage: "courseStructure",
     status: "inProgress",
   },
 ];
+
+//sort info by lastUpdated date:
+const sortedInfo = info.sort(
+  (a, b) => new Date(b.lastUpdated) - new Date(a.lastUpdated)
+);
 
 const RequestsTable = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -52,7 +57,7 @@ const RequestsTable = () => {
   if (width < 1000) {
     return (
       <section className="requests-table-small">
-        {info.map((request, index) => (
+        {sortedInfo.map((request, index) => (
           <RequestRowSmallScreen key={index} request={request} />
         ))}
       </section>
@@ -71,7 +76,7 @@ const RequestsTable = () => {
         </tr>
       </thead>
       <tbody>
-        {info.map((request, index) => (
+        {sortedInfo.map((request, index) => (
           <RequestRow key={index} request={request} />
         ))}
       </tbody>
